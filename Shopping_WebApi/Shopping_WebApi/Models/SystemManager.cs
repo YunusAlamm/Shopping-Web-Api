7721b19e3c7765d7f4ff1c9a84b6f525869c598b
YunusAlamm <alamdari.yoonos@gmail.com>
2024-06-13 12:03:22 +0330

    Create SystemManager.cs

---
+++ Shopping_WebApi/Shopping_WebApi/Models/SystemManager.cs
@@ -0,0 +1,14 @@
+﻿namespace Shopping_WebApi.Models
+{
+    public class SystemManager : User
+    {
+        public List<StoreManager> StoreManagers { get; set; }
+        public SystemManager() 
+        {
+            StoreManagers = new List<StoreManager>();
+        }
+        public void AddStoreManager(StoreManager manager) { StoreManagers.Add(manager); }
+        public void RemoveStoreManager(StoreManager manager) { StoreManagers.Remove(manager); }
+
+    }
+}
