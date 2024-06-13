01655932ca3082039878d6e84829ab175b1888b2
YunusAlamm <alamdari.yoonos@gmail.com>
2024-06-13 12:02:45 +0330

    Create PhysicalProduct.cs

---
+++ Shopping_WebApi/Shopping_WebApi/Models/PhysicalProduct.cs
@@ -0,0 +1,9 @@
+﻿namespace Shopping_WebApi.Models
+{
+    public class PhysicalProduct : Product
+    {
+        public double Weight { get; set; }
+        public string Dimensions { get; set; }
+        public bool IsReturnable { get; set; }
+    }
+}
