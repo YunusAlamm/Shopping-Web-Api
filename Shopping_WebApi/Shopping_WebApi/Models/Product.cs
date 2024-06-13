412cfe179a49ed3c0034f8953d0829d6af5bd33c
YunusAlamm <alamdari.yoonos@gmail.com>
2024-06-13 12:02:49 +0330

    Create Product.cs

---
+++ Shopping_WebApi/Shopping_WebApi/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿namespace Shopping_WebApi.Models
+{
+    public abstract class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Category Category { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public Cart Cart { get; set; }
+        public List<ProductAttribute> Attributes { get; set; }
+
+        public Product()
+        {
+            Attributes = new List<ProductAttribute>(); //add custom attributes to each product without needing to modify the database schema or the class definition every time.
+        }
+    }
+
+    public  class ProductAttribute
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+}
