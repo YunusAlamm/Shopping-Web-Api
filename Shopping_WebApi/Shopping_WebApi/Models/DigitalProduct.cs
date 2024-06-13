dbd08e845d938d9e36a7282a9fd7172a616481bf
YunusAlamm <alamdari.yoonos@gmail.com>
2024-06-13 12:02:20 +0330

    Create DigitalProduct.cs

---
+++ Shopping_WebApi/Shopping_WebApi/Models/DigitalProduct.cs
@@ -0,0 +1,9 @@
+﻿namespace Shopping_WebApi.Models
+{
+    public class DigitalProduct : Product
+    {
+        public string DigitalFormat { get; set; } // PDF,MP3,MP4,...
+        public string DownloadLink { get; set; }
+        public int LicenseKey { get; set; }
+    }
+}
