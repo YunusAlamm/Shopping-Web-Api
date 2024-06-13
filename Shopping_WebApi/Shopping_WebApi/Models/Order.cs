e7c743389432bb2eae8a0735d34f5455da4d3767
YunusAlamm <alamdari.yoonos@gmail.com>
2024-06-13 12:02:39 +0330

    Create Order.cs

---
+++ Shopping_WebApi/Shopping_WebApi/Models/Order.cs
@@ -0,0 +1,17 @@
+﻿namespace Shopping_WebApi.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public RegularUser Owner { get; set; }
+        public DateTime OrderDate { get; set; }
+        public List<Product> Products { get; set; }
+        public decimal TotalAmount { get; set; }
+
+
+        public Order()
+        {
+            Products = new List<Product>();
+        }
+    }
+}
