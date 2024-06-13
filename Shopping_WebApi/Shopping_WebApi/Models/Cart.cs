6199df07e3f69beacf0320f3adb678039e87459d
YunusAlamm <alamdari.yoonos@gmail.com>
2024-06-13 12:02:10 +0330

    Create Cart.cs

---
+++ Shopping_WebApi/Shopping_WebApi/Models/Cart.cs
@@ -0,0 +1,15 @@
+﻿namespace Shopping_WebApi.Models
+{
+    public class Cart
+    {
+        
+        public int Id { get; set; }
+        public RegularUser Owner { get; set; }
+        public List<Product> Products { get; set; }
+        public Cart() { Products = new List<Product>(); }
+        public void AddToProducts(Product product)
+        {
+            Products.Add(product);
+        }
+    }
+}
