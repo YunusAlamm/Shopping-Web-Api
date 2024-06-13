bd273cc28f91ce0d58a924e14da574f9d54fb266
YunusAlamm <alamdari.yoonos@gmail.com>
2024-06-13 12:03:25 +0330

    Create User.cs

---
+++ Shopping_WebApi/Shopping_WebApi/Models/User.cs
@@ -0,0 +1,18 @@
+﻿namespace Shopping_WebApi.Models
+{
+    public abstract class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+
+        private string _PhoneNumber;
+        private int _MaxLength = 11;
+        public string PhoneNumber
+        {
+            get => _PhoneNumber;
+            set => _PhoneNumber = value.Length <= _MaxLength ? value : value.Substring(0, _MaxLength);
+        }
+        public string Email {  get; set; }
+    }
+}
