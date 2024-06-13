5ed432825a5b7bfbabaaadccdd96a5c3730e5425
YunusAlamm <alamdari.yoonos@gmail.com>
2024-06-13 12:02:53 +0330

    Create RegularUser.cs

---
+++ Shopping_WebApi/Shopping_WebApi/Models/RegularUser.cs
@@ -0,0 +1,31 @@
+﻿namespace Shopping_WebApi.Models
+{
+    public  class RegularUser : User
+    {
+        public string Address { get; set; }
+        public string PostCode { get; set; }
+        public Cart Cart { get; set; }
+
+        public List<Order> OrderHistory { get; set; }
+        public List<Product> WishList { get; set; }
+        public RegularUser() 
+        {
+            WishList = new List<Product>();
+            OrderHistory = new List<Order>();
+        }
+        public void AddToWishList(Product product)
+        {
+            WishList.Add(product);
+        }
+
+        public void AddOrderToHistory(Order order)
+        {
+            OrderHistory.Add(order);
+        }
+
+      
+    
+
+
+}
+}
