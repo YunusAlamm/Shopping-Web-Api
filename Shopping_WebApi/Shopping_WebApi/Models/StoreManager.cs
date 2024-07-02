23588d0f4d3cbe019d007d1a41a17ce92be6dd48
YunusAlamm <alamdari.yoonos@gmail.com>
2024-07-02 18:46:47 +0330

    Update StoreManager.cs

--- Shopping_WebApi/Shopping_WebApi/Models/StoreManager.cs
+++ Shopping_WebApi/Shopping_WebApi/Models/StoreManager.cs
@@ -1,57 +1,72 @@
-﻿namespace Shopping_WebApi.Models
+﻿
+
+namespace Shopping_WebApi.Models
 {
     public class StoreManager : User
     {
-       
-        public List<Order> PendingOrders { get; set; }
-        public List<Product> Inventory { get; set; }
-        public Dictionary<Product, int> StockLevels { get; set; }
+        public SystemManager.Role ManagerRole { get; set; }
 
+        public List<Order> PendingOrders { get; set; }
+        public List<Order> CompletedSales { get; set; }
+        public Dictionary<Product, int> Inventory { get; set; }
+       
         // Constructor
         public StoreManager()
         {
             PendingOrders = new List<Order>();
-            Inventory = new List<Product>();
-            StockLevels = new Dictionary<Product, int>();
+            
+            Inventory = new Dictionary<Product, int>();
+            CompletedSales = new List<Order>();
         }
 
         // Methods specific to StoreManager
         public void ProcessOrder(Order order)
         {
-            // Implement logic to process the order
+            
             //بعدا باید اضافه کنم وقتی پرداختش موفقیت آمیز بود سفارش نهایی بشه
             PendingOrders.Remove(order);
+            CompletedSales.Add(order);
+            
+
+
             // Update inventory, etc.
         }
 
-        public void UpdateStockLevel(Product product, int quantity)
-        {
-            // Implement logic to update stock levels
-            if (StockLevels.ContainsKey(product))
-            {
-                StockLevels[product] = quantity;
-            }
-            else
-            {
-                StockLevels.Add(product, quantity);
-            }
-        }
+      
+       
 
-        public void AddProductToInventory(Product product)
+
+        public void AddProductToInventory(Product product, int quantity = 1)
         {
             // Implement logic to add a product to the inventory
-            Inventory.Add(product);
-            // Assume a default stock level for a new product
-            StockLevels.Add(product, 0);
+            Inventory.Add(product,quantity);
+            
         }
 
-        public void RemoveProductFromInventory(Product product)
+        public void UpdateStockAfterSale(Order order)
         {
-            // Implement logic to remove a product from the inventory
-            Inventory.Remove(product);
-            StockLevels.Remove(product);
+            foreach (var product in order.Products)
+            {
+                // Check if the product exists in the inventory before attempting to adjust it
+                if (Inventory.ContainsKey(product.Key))
+                {
+                    
+                    // Decrement the quantity
+                    Inventory[product.Key]--;
+
+                    // If the quantity reaches 0, remove the product from the inventory
+                    if (Inventory[product.Key] == 0)
+                    {
+                        Inventory.Remove(product.Key);
+                    }
+
+
+                }
+            }
         }
 
-        // Additional methods can be added as needed
+
+
+
     }
 }
