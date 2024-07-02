858d335f13b86b5834a44ea27a0ff08f76d2e816
YunusAlamm <alamdari.yoonos@gmail.com>
2024-07-02 18:46:20 +0330

    Create Shopping_StoreContext.cs

---
+++ Shopping_WebApi/Shopping_WebApi/Data/Shopping_StoreContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shopping_WebApi.Models; 
+
+namespace Shopping_WebApi.Data
+{
+    public class Shopping_StoreContext : DbContext
+    {
+        public Shopping_StoreContext(DbContextOptions<Shopping_StoreContext> options)
+            : base(options)
+        {}
+        public DbSet<User> Users { get; set; } 
+        public DbSet<RegularUser> RegularUsers { get; set; } 
+        public DbSet<StoreManager> StoreManagers { get; set; } 
+        public DbSet<SystemManager> SystemManagers { get; set; } 
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; } 
+        public DbSet<Cart> Carts { get; set; } 
+
+        
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<DigitalProduct> DigitalProducts { get; set; }
+        public DbSet<PhysicalProduct> PhysicalProducts { get; set; }
+
+        
+
+        
+    }
+}
