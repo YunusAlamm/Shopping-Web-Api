49cc579f316db4ad2f309d67369c84e26b131f68
YunusAlamm <alamdari.yoonos@gmail.com>
2024-08-16 15:08:48 +0330

    Create Costumer.cs

---
+++ Shopping_WebApi/Shopping_WebApi/Models/Costumer.cs
@@ -0,0 +1,18 @@
+﻿namespace Shopping_WebApi.Models
+{
+    public  class Costumer : User
+    {
+        public string Address { get; set; }
+        public string PostCode { get; set; }
+        public Cart Cart { get; set; }
+
+        public List<Order> OrderHistory { get; set; }
+        public List<Product> WishList { get; set; }
+     
+
+      
+    
+
+
+}
+}
