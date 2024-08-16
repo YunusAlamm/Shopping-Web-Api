156b4f4c442e6742792fb7c2cbe42a7084eb4ac9
YunusAlamm <alamdari.yoonos@gmail.com>
2024-08-16 15:08:50 +0330

    Update Order.cs

--- Shopping_WebApi/Shopping_WebApi/Models/Order.cs
+++ Shopping_WebApi/Shopping_WebApi/Models/Order.cs
@@ -3,15 +3,12 @@
     public class Order //when completing the purchase of cart, the detail of cart become an order and saves in orderHistory of user.
     {
         public int Id { get; set; }
-        public RegularUser Owner { get; set; }
+        public Costumer Owner { get; set; }
         public DateTime OrderDate { get; set; }
         public Dictionary<Product, int> Products { get; set; }
         public decimal TotalAmount { get; set; }
 
 
-        public Order()
-        {
-            Products = new Dictionary<Product, int>();
-        }
+        
     }
 }
