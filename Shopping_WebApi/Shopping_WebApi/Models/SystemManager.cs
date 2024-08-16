4b5cb961da769fb274eeec7aa0d28759679661a8
YunusAlamm <alamdari.yoonos@gmail.com>
2024-08-16 15:09:04 +0330

    Update SystemManager.cs

--- Shopping_WebApi/Shopping_WebApi/Models/SystemManager.cs
+++ Shopping_WebApi/Shopping_WebApi/Models/SystemManager.cs
@@ -12,33 +12,5 @@ public class SystemManager : User
 
     public List<StoreManager> StoreManagers { get; set; }
 
-    public SystemManager()
-    {
-        StoreManagers = new List<StoreManager>();
-    }
-
-    public void AddStoreManager(StoreManager manager)
-    {
-        StoreManagers.Add(manager);
-    }
-
-    public void RemoveStoreManager(StoreManager manager)
-    {
-        StoreManagers.Remove(manager);
-    }
-
-    // Assign a role to a store manager
-    public void AssignRoleToStoreManager(StoreManager manager, Role role = Role.Basic) // the default Role of store manager is basic, so they can only proccess orders
-    {
-        if (StoreManagers.Contains(manager))
-        {
-            manager.ManagerRole = role;
-        }
-    }
-
-    // Check if a store manager has the required role to perform an action
-    public bool CanPerformAction(StoreManager manager, Role requiredRole)
-    {
-        return manager.ManagerRole >= requiredRole;
-    }
+   
 }
