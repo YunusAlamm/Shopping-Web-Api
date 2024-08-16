9a72d33502c4af4b24a572266a26feb690b15ec8
YunusAlamm <alamdari.yoonos@gmail.com>
2024-08-16 15:08:41 +0330

    Update Cart.cs

--- Shopping_WebApi/Shopping_WebApi/Models/Cart.cs
+++ Shopping_WebApi/Shopping_WebApi/Models/Cart.cs
@@ -4,23 +4,10 @@
     {
         
         public int Id { get; set; }
-        public RegularUser Owner { get; set; }
+        public Costumer Owner { get; set; }
         public Dictionary<Product,int> Products { get; set; }
         public Cart() { Products = new Dictionary<Product, int>(); }
-        public void AddToProducts(Product product, int quantity)
-        {
-            // Check if the dictionary already contains the product
-            if (Products.ContainsKey(product))
-            {
-                // If it does, update the quantity
-                Products[product] += quantity;
-            }
-            else
-            {
-                // If it doesn't, add the product with the specified quantity
-                Products.Add(product, quantity);
-            }
-        }
+     
 
 
     }
