0adb9f0229548b0987e37628c3e16f1ba28ba468
YunusAlamm <alamdari.yoonos@gmail.com>
2024-08-20 20:12:25 +0330

    Update Order.cs

--- Shopping_WebApi/Shopping_WebApi/Models/Order.cs
+++ Shopping_WebApi/Shopping_WebApi/Models/Order.cs
@@ -3,14 +3,18 @@
     public class Order //when completing the purchase of cart, the detail of cart become an order and saves in orderHistory of user.
     {
         public Guid Id { get; set; }
-        public Customer Owner { get; set; }
-        public DateTime OrderDate { get; set; }
-        
+               
         public decimal TotalAmount { get; set; }
 
         public bool IsCompleted { get; set; }
+        public DateTime PurchaseTime { get; set; }
+
+        public ICollection<OrderProduct> Products { get; set; }
+
+        public string CustomerId { get; set; }
+        public Customer Customer { get; set; }
 
 
-        
+
     }
 }
