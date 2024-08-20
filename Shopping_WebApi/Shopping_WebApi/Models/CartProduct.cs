c223c03dc9c7451f0d059588020261ac5a192760
YunusAlamm <alamdari.yoonos@gmail.com>
2024-08-20 20:12:21 +0330

    Update CartProduct.cs

--- Shopping_WebApi/Shopping_WebApi/Models/CartProduct.cs
+++ Shopping_WebApi/Shopping_WebApi/Models/CartProduct.cs
@@ -2,10 +2,11 @@
 {
     public class CartProduct
     {
-        public int CartId { get; set; }
+        public Guid Id { get; set; }
+        public Guid CartId { get; set; }
         public Cart Cart { get; set; }
 
-        public int ProductId { get; set; }
+        public Guid ProductId { get; set; }
         public Product Product { get; set; }
 
         public int Quantity { get; set; }
