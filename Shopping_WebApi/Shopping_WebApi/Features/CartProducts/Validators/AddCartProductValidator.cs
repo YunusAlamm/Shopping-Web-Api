8525a813a085b22a7f9a91e2fd0dffb2eebb3d84
Yunus Alamdari <alamdari.yoonos@gmail.com>
2024-08-22 09:27:57 +0330

    Create AddCartProductValidator.cs

---
+++ Shopping_WebApi/Shopping_WebApi/Features/CartProducts/Validators/AddCartProductValidator.cs
@@ -0,0 +1,36 @@
+﻿using FluentValidation;
+using Shopping_WebApi.Features.CartProduct.Commands;
+
+namespace Shopping_WebApi.Features.CartProducts.Validators
+{
+
+
+    public class AddCartProductValidator : AbstractValidator<AddCartProductCommand>
+    {
+        public AddCartProductValidator()
+        {
+            RuleFor(x => x.CartId)
+                .NotEmpty().NotNull().WithMessage("CartId is required.");
+
+            RuleFor(x => x.ProductId)
+                .NotEmpty().WithMessage("ProductId is required.");
+
+            RuleFor(x => x.Quantity)
+                .GreaterThan(0).NotNull().WithMessage("Quantity must be greater than zero.");
+        }
+    }
+
+    public class DeleteCartProductValidator : AbstractValidator<DeleteCartProductCommand>
+    {
+        public DeleteCartProductValidator()
+        {
+            RuleFor(x => x.Id)
+                .NotEmpty().NotNull().WithMessage("Id is required.");
+
+           
+        }
+    }
+
+}
+
+
