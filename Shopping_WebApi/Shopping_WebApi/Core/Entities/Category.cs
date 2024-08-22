8c0a50f8140e56093a7ea4e491bf59db6272795b
Yunus Alamdari <alamdari.yoonos@gmail.com>
2024-08-22 09:26:31 +0330

    Update Category.cs

--- Shopping_WebApi/Shopping_WebApi/Core/Entities/Category.cs
+++ Shopping_WebApi/Shopping_WebApi/Core/Entities/Category.cs
@@ -4,6 +4,6 @@
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
-        public List<Product> Products { get; set; }
+        public List<Product>? Products { get; set; }
     }
 }
