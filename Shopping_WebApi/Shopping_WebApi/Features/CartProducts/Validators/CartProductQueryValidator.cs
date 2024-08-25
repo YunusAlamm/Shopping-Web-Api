0692536c47b0593c92b779e7de9eb32eca4c08b5
YunusAlamm <alamdari.yoonos@gmail.com>
2024-08-25 09:30:46 +0330

    Create CartProductQueryValidator.cs

---
+++ Shopping_WebApi/Shopping_WebApi/Features/CartProducts/Validators/CartProductQueryValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using Shopping_WebApi.Features.CartProducts.Queries;
+
+namespace Shopping_WebApi.Features.CartProducts.Validators
+{
+    public class CartProductQueryValidator: AbstractValidator<CartProductQuery>
+    {
+        public CartProductQueryValidator() 
+        {
+            RuleFor(x => x.Id)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("Id is required.");
+        }
+
+    }
+
+}
+
+
