1fdd12807b6ba79717f7806182ddafa0ceb59f59
YunusAlamm <alamdari.yoonos@gmail.com>
2024-08-25 08:44:53 +0330

    Update CartDto.cs

--- Shopping_WebApi/Shopping_WebApi/Features/Carts/Dtos/CartDto.cs
+++ Shopping_WebApi/Shopping_WebApi/Features/Carts/Dtos/CartDto.cs
@@ -5,8 +5,7 @@ namespace Shopping_WebApi.Features.Carts.Dtos
     public class CartDto
     {
         public Guid Id { get; set; }
-        public string CustomerId { get; set; }
-        public Customer Customer { get; set; }
+        
 
         public ICollection<Cart_Product> Products { get; set; }
         public decimal TotalAmount { get; set; }
