4d5b8beb7e91d8b86e0d2703a50bf8677a16e068
YunusAlamm <alamdari.yoonos@gmail.com>
2024-08-25 09:30:49 +0330

    Create DeleteCartProductValidator.cs

---
+++ Shopping_WebApi/Shopping_WebApi/Features/CartProducts/Validators/DeleteCartProductValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using Shopping_WebApi.Features.CartProduct.Commands;
+
+namespace Shopping_WebApi.Features.CartProducts.Validators
+{
+    public class DeleteCartProductValidator : AbstractValidator<DeleteCartProductCommand>
+    {
+        public DeleteCartProductValidator()
+        {
+            RuleFor(x => x.Id)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("Id is required.");
+
+           
+        }
+    }
+
+}
+
+
