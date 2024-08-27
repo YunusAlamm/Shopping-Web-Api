164ea9d67673757a2ffff20928a0fec2267fb7ec
YunusAlamm <alamdari.yoonos@gmail.com>
2024-08-27 14:28:02 +0330

    Create CategoriesQueryHandler.cs

---
+++ Shopping_WebApi/Shopping_WebApi/Features/Category/QueryHandlers/CategoriesQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using MediatR;
+using Shopping_WebApi.Features.Categories.Dto;
+using Shopping_WebApi.Features.Categories.Queries;
+using Shopping_WebApi.Infrastructure.Repositories;
+
+namespace Shopping_WebApi.Features.Categories.QueryHandlers
+{
+    public class CategoriesQueryHandler(
+        IGenericRepository<Core.Entities.Category> _genericRepository,
+        IMapper _mapper
+        ) : IRequestHandler<CategoriesQuery,IEnumerable<CategoryDto>>
+    {
+        
+
+        public async Task<IEnumerable<CategoryDto>> Handle(CategoriesQuery request, CancellationToken cancellationToken)
+        {
+
+            var categories = await _genericRepository.GetAllAsync();
+
+            return _mapper.Map<IEnumerable<CategoryDto>>(categories);
+        }
+    }
+}
