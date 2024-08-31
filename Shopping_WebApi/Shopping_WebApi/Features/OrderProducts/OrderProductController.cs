1cc618abac32723470503c175181177466bf873f
YunusAlamm <alamdari.yoonos@gmail.com>
2024-08-31 14:15:33 +0330

    Create OrderProductController.cs

---
+++ Shopping_WebApi/Shopping_WebApi/Features/OrderProducts/OrderProductController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Shopping_WebApi.Features.OrderProducts
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderProductController : ControllerBase
+    {
+        // GET: api/<OrderProductController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<OrderProductController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<OrderProductController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<OrderProductController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<OrderProductController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
