c8d14a7aead1d4b3f3e1ab77f9c728341a4a0787
YunusAlamm <alamdari.yoonos@gmail.com>
2024-09-02 16:37:47 +0330

    Create CommentRepository.cs

---
+++ Shopping_WebApi/Shopping_WebApi/Infrastructures/Repositories/CommentRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shopping_WebApi.Core.Entities;
+using Shopping_WebApi.Infrastructure.Repositories;
+
+namespace Shopping_WebApi.Infrastructures.Repositories
+{
+    public class CommentRepository(DbContext _context) : GenericRepository<Comment>(_context)
+    {
+        
+        public async Task<IEnumerable<Comment>> GetCommentsByProductIdAsync(Guid ProductId)
+        {
+            return await _dbSet.Where(c =>  c.ProductId == ProductId).ToListAsync();
+        }
+
+
+        public async Task<IEnumerable<Comment>> GetCommentsByUserIdAsync(Guid UserId)
+        {
+            return await _dbSet.Where(c => c.UserId == UserId).ToListAsync();
+        }
+    }
+}
