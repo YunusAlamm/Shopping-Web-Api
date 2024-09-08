89e42c3c78479b47cd53b426ed546540fdbb89a3
YunusAlamm <alamdari.yoonos@gmail.com>
2024-09-08 21:40:03 +0330

    registering services

--- Shopping_WebApi/Shopping_WebApi/Program.cs
+++ Shopping_WebApi/Shopping_WebApi/Program.cs
@@ -1,29 +1,45 @@
+using FluentValidation;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Shopping_WebApi.Core.Entities;
+using Shopping_WebApi.Features.Categories.Mapping;
+using Shopping_WebApi.Features.Category.Commands;
+using Shopping_WebApi.Features.Category.Validators;
 using Shopping_WebApi.Infrastructure.Data.DbContext;
 using Shopping_WebApi.Infrastructure.Repositories;
+using Shopping_WebApi.Infrastructures.Repositories;
+using System.Reflection;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
 
 
-var ConectionString = builder.Configuration.GetConnectionString("ShoppingStore");
-builder.Services.AddDbContext<Shopping_StoreContext>(options => options.UseNpgsql(ConectionString));
+var conectionString = builder.Configuration.GetConnectionString("ShoppingStore");
+builder.Services.AddDbContext<Shopping_StoreContext>(options =>
+    options.UseNpgsql(conectionString));
 
 
 builder.Services.AddIdentityApiEndpoints<User>()
     .AddEntityFrameworkStores<Shopping_StoreContext>();
+
 builder.Services.AddAuthentication(IdentityConstants.BearerScheme);
 
 
-builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+
 
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddScoped(typeof(ICommentRepository), typeof(CommentRepository));
+builder.Services.AddAutoMapper(typeof(CategoryMapping));
+builder.Services.AddValidatorsFromAssemblyContaining(typeof(AddCategoryCommandValidator));
+//builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+builder.Services.AddMediatR(x => x.RegisterServicesFromAssemblyContaining(typeof(AddCategoryCommandHandler)));
 
 var app = builder.Build();
 
