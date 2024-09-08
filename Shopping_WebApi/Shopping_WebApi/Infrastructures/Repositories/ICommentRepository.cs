8853a43aefaea4b48781d1714139e21325943406
YunusAlamm <alamdari.yoonos@gmail.com>
2024-09-08 21:40:09 +0330

    Create ICommentRepository.cs

---
+++ Shopping_WebApi/Shopping_WebApi/Infrastructures/Repositories/ICommentRepository.cs
@@ -0,0 +1,11 @@
+﻿using Shopping_WebApi.Core.Entities;
+using Shopping_WebApi.Infrastructure.Repositories;
+
+namespace Shopping_WebApi.Infrastructures.Repositories
+{
+    public interface ICommentRepository: IGenericRepository<Comment>
+    {
+        Task<IEnumerable<Comment>> GetCommentsByProductIdAsync(Guid ProductId);
+        Task<IEnumerable<Comment>> GetCommentsByUserIdAsync(Guid UserId);
+    }
+}
