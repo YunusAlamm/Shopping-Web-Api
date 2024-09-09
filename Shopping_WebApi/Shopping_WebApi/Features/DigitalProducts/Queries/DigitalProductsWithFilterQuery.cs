764a20058bd4cffa00dac903e8c8fb06835c8d32
YunusAlamm <alamdari.yoonos@gmail.com>
2024-09-09 12:18:52 +0330

    implement query with filters for digital products

---
+++ Shopping_WebApi/Shopping_WebApi/Features/DigitalProducts/Queries/DigitalProductsWithFilterQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using Shopping_WebApi.Features.DigitalProducts.Dto;
+
+
+namespace Shopping_WebApi.Features.DigitalProducts.Queries
+{
+    public class DigitalProductsWithFilterQuery : IRequest<List<DigitalProductDto>> 
+    {
+        public string Genre { get; set; }
+        public List<Core.Entities.Category> Categories { get; set; }
+        public string DigitalFormat { get; set; }
+    }
+
+}
---
+++ Shopping_WebApi/Shopping_WebApi/Features/DigitalProducts/QueryHandlers/DigitalProductsWithFilterQueryHandler.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using MediatR;
+using NinjaNye.SearchExtensions;
+using Shopping_WebApi.Core.Entities;
+using Shopping_WebApi.Features.DigitalProducts.Dto;
+using Shopping_WebApi.Features.DigitalProducts.Queries;
+using Shopping_WebApi.Infrastructure.Repositories;
+
+namespace Shopping_WebApi.Features.DigitalProducts.QueryHandlers
+{
+    public class DigitalProductsWithFilterQueryHandler(
+        IGenericRepository<DigitalProduct> _genericRepository,
+        IMapper _mapper
+        ) : IRequestHandler<DigitalProductsWithFilterQuery, List<DigitalProductDto>>
+    {
+        public async Task<List<DigitalProductDto>> Handle(DigitalProductsWithFilterQuery request, CancellationToken cancellationToken)
+        {
+            var Products = await _genericRepository.GetAllAsync();
+           var digitalProducts = Products.Search(
+               p => p.Genre
+               ).Containing(request.Genre);
+
+            digitalProducts = (EnumerableStringSearch<DigitalProduct>)Products
+                .Where(
+                p => p.Categories
+                .Any(
+                    c => request.Categories
+                    .Contains(c))
+                );
+
+            digitalProducts = Products
+                .Search(
+                p => p.DigitalFormat)
+                .Containing(
+                request.DigitalFormat);
+
+
+            return _mapper.Map<List<DigitalProductDto>>(digitalProducts);
+        }
+    }
+}
