a45bfe25c41e6a23f70146b36652752fa0ae9d1a
YunusAlamm <alamdari.yoonos@gmail.com>
2024-09-22 06:39:42 +0330

    implement payment infrastructure

---
+++ Shopping_WebApi/Shopping_WebApi/Infrastructures/ZarinPalGateway/IZarinpalService.cs
@@ -0,0 +1,12 @@
+﻿using Shopping_WebApi.Core.Entities;
+
+
+namespace Shopping_WebApi.Infrastructures.ZarinPalGateway
+{
+    public interface IZarinpalService
+    {
+        Task<string> RequestToPay(RequestToPay request);
+        Task<int> Validate(RequestToValidate request);
+
+    }
+}
---
+++ Shopping_WebApi/Shopping_WebApi/Infrastructures/ZarinPalGateway/ZarinpalService.cs
@@ -0,0 +1,45 @@
+﻿
+using Dto.Payment;
+using Shopping_WebApi.Core.Entities;
+using ZarinPal.Class;
+
+
+namespace Shopping_WebApi.Infrastructures.ZarinPalGateway
+{
+    public class ZarinpalService(
+        Payment _payment
+        ) : IZarinpalService
+    {
+        public async Task<string> RequestToPay(RequestToPay request)
+        {
+
+            var result = await _payment.Request(new DtoRequest()
+            {
+                MerchantId = request.MerchantId,
+                Amount = request.Amount,
+                Description = request.Description,
+                CallbackUrl = request.CallbackUrl,
+                Email = request.Email,
+                Mobile = request.Mobile
+
+
+            }, Payment.Mode.sandbox);
+
+
+            return result.Authority;
+        }
+
+        public async Task<int> Validate(RequestToValidate request)
+        {
+            var verificationResult = await _payment.Verification(new DtoVerification()
+            {
+                MerchantId = "",
+                Amount = request.Amount,
+                Authority = request.Authority
+            }, Payment.Mode.sandbox);
+
+
+            return verificationResult.RefId;
+        }
+    }
+}
