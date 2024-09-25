8c5769bf85cf8ba1177c491a243c1cff5afc04fb
YunusAlamm <alamdari.yoonos@gmail.com>
2024-09-25 06:22:46 +0330

    Create RequestToPay.cs

---
+++ Shopping_WebApi/Shopping_WebApi/Core/Models/RequestToPay.cs
@@ -0,0 +1,14 @@
+﻿namespace Shopping_WebApi.Core.Models
+{
+    public class RequestToPay
+    {
+        public Guid Id { get; set; }
+
+        public string MerchantId { get; set; }
+        public int Amount { get; set; }
+        public string Description { get; set; }
+        public string CallbackUrl { get; set; }
+        public string? Mobile { get; set; }
+        public string? Email { get; set; }
+    }
+}
