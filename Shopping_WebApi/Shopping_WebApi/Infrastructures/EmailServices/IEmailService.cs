af9a9b5ba8b80b8c2ba8b20e41b6dfda567aefa8
YunusAlamm <alamdari.yoonos@gmail.com>
2024-09-25 06:26:10 +0330

    add sending email service

---
+++ Shopping_WebApi/Shopping_WebApi/Infrastructures/EmailServices/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿namespace Shopping_WebApi.Infrastructures.EmailServices
+{
+    public interface IEmailService
+    {
+        Task SendEmailAsync(string toName, string subject, string body, string toEmail);
+    }
+
+    
+}
