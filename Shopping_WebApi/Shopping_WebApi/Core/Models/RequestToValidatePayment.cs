487469e7942f1117201ff00c020d1a24e052bc6d
YunusAlamm <alamdari.yoonos@gmail.com>
2024-09-25 06:22:54 +0330

    Create RequestToValidatePayment.cs

---
+++ Shopping_WebApi/Shopping_WebApi/Core/Models/RequestToValidatePayment.cs
@@ -0,0 +1,8 @@
+﻿namespace Shopping_WebApi.Core.Models
+{
+    public class RequestToValidatePayment
+    {
+        public int Amount { get; set; }
+        public string Authority { get; set; }
+    }
+}
