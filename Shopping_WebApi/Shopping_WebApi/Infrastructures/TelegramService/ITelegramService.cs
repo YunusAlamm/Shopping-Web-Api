926cc203fe32f41898ee885fbb6f3ac4bb7ab858
YunusAlamm <alamdari.yoonos@gmail.com>
2024-09-25 06:26:33 +0330

    add telegram service

---
+++ Shopping_WebApi/Shopping_WebApi/Infrastructures/TelegramService/ITelegramService.cs
@@ -0,0 +1,8 @@
+﻿namespace Shopping_WebApi.Infrastructures.TelegramService
+{
+    public interface ITelegramService
+    {
+        Task SendMessage(string message);
+    }
+
+}
---
+++ Shopping_WebApi/Shopping_WebApi/Infrastructures/TelegramService/TelegramService.cs
@@ -0,0 +1,20 @@
+﻿using Shopping_WebApi.Core.Models;
+using Telegram.Bot.Types;
+using Telegram.Bot;
+
+namespace Shopping_WebApi.Infrastructures.TelegramService
+{
+    public class TelegramService(
+    TelegramBotConfiguration config
+    ) : ITelegramService
+    {
+        private readonly TelegramBotClient _botClient = new TelegramBotClient(config.BotToken);
+        private readonly string _channelId = config.ChannelId;
+
+        public async Task SendMessage(string message)
+        {
+            await _botClient.SendTextMessageAsync(new ChatId(_channelId), message);
+        }
+    }
+
+}
