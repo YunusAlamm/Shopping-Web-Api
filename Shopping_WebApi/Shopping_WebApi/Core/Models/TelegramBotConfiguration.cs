82b279316240944297902660c55d9b4175f1d362
YunusAlamm <alamdari.yoonos@gmail.com>
2024-09-25 06:22:58 +0330

    Create TelegramBotConfiguration.cs

---
+++ Shopping_WebApi/Shopping_WebApi/Core/Models/TelegramBotConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace Shopping_WebApi.Core.Models
+{
+    public class TelegramBotConfiguration
+    {
+        public string BotToken { get; set; }
+        public string ChannelId { get; set; }
+    }
+}
