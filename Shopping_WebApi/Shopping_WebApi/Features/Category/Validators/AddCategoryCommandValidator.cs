fb43895cb544e4383f93215e5b44e2303f6911fe
YunusAlamm <alamdari.yoonos@gmail.com>
2024-10-30 13:21:55 +0330

    add feature for checking the similar Categories

--- Shopping_WebApi/Shopping_WebApi/Features/Category/Validators/AddCategoryCommandValidator.cs
+++ Shopping_WebApi/Shopping_WebApi/Features/Category/Validators/AddCategoryCommandValidator.cs
@@ -1,19 +1,27 @@
 ﻿using FluentValidation;
 using Shopping_WebApi.Features.Category.Commands;
+using Shopping_WebApi.Infrastructure.Repositories;
 
 namespace Shopping_WebApi.Features.Category.Validators
 {
-    public class AddCategoryCommandValidator: AbstractValidator<AddCategoryCommand>
+    public class AddCategoryCommandValidator : AbstractValidator<AddCategoryCommand>
     {
-        public AddCategoryCommandValidator() 
+        private readonly IGenericRepository<Core.Entities.Category> _categoryRepository;
+
+        public AddCategoryCommandValidator(IGenericRepository<Core.Entities.Category> categoryRepository)
         {
+            _categoryRepository = categoryRepository;
+
             RuleFor(x => x.Name)
                 .NotEmpty()
                 .NotNull()
-                .WithMessage("Choose a name for your Category!");
-        
-            
-        
+                .WithMessage("Choose a name for your Category!")
+                .MustAsync(BeUniqueName).WithMessage("A category with the same name already exists.");
         }
-    } 
+
+        private async Task<bool> BeUniqueName(string name, CancellationToken cancellationToken)
+        {
+            return !await _categoryRepository.GetByConditions(c => c.Name == name);
+        }
+    }
 }
