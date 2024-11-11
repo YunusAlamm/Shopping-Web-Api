42cb21b4557d7ff20f092efd512ee8e3215a421c
YunusAlamm <alamdari.yoonos@gmail.com>
2024-11-11 16:16:50 +0330

    Update Product.cs

--- Shopping_WebApi/Shopping_WebApi/Core/Entities/Product.cs
+++ Shopping_WebApi/Shopping_WebApi/Core/Entities/Product.cs
@@ -4,16 +4,16 @@
     {
         
         public Guid Id { get; set; }
-        public string Name { get; set; }
-        public byte[] Image { get; set; }
-        public List<Category> Categories { get; set; }
+        public string? Name { get; set; }
+        public byte[]? Image { get; set; }
+        public List<Category>? Categories { get; set; }
         public decimal Price { get; set; }
-        public string Description { get; set; }
+        public string? Description { get; set; }
         public bool? IsSoldOut { get; set; }
         
 
 
-        public Dictionary<string, string> Attributes { get; set; }
+        public Dictionary<string, string>? Attributes { get; set; }
 
 
     }
