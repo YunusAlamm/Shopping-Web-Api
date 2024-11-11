a784736196049fe4a667f1b0d1ef4a2c07ee20f7
YunusAlamm <alamdari.yoonos@gmail.com>
2024-11-11 16:16:40 +0330

    Update OrderProduct.cs

--- Shopping_WebApi/Shopping_WebApi/Core/Entities/OrderProduct.cs
+++ Shopping_WebApi/Shopping_WebApi/Core/Entities/OrderProduct.cs
@@ -4,10 +4,10 @@
     {
         public Guid Id { get; set; }
         public Guid OrderId { get; set; }
-        public Order Order { get; set; }
+        public Order? Order { get; set; }
 
         public Guid ProductId { get; set; }
-        public Product Product { get; set; }
+        public Product? Product { get; set; }
 
         public int QuantityOfProduct { get; set; }
     }
