61751c7c41a17b5f693c14f61b6ac42037e1cc8c
YunusAlamm <alamdari.yoonos@gmail.com>
2024-11-11 16:19:02 +0330

    Update Shopping_StoreContext.cs

--- Shopping_WebApi/Shopping_WebApi/Infrastructures/Contexts/DbContext/Shopping_StoreContext.cs
+++ Shopping_WebApi/Shopping_WebApi/Infrastructures/Contexts/DbContext/Shopping_StoreContext.cs
@@ -4,20 +4,22 @@ using Shopping_WebApi.Core.Entities;
 
 namespace Shopping_WebApi.Infrastructure.Data.DbContext
 {
-    public class Shopping_StoreContext(DbContextOptions<Shopping_StoreContext> options) : IdentityDbContext<User>(options)
+    public class Shopping_StoreContext : IdentityDbContext<User>
     {
-        public override DbSet<User> Users { get; set; }
+        public Shopping_StoreContext(DbContextOptions<Shopping_StoreContext> options)
+            : base(options)
+        {
+        }
+
         public DbSet<Customer> Customers { get; set; }
         public DbSet<StoreManager> StoreManagers { get; set; }
         public DbSet<SystemManager> SystemManagers { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<Order> Orders { get; set; }
         public DbSet<Cart> Carts { get; set; }
-        public DbSet<Cart_Product> CartProducts { get; set; }
+        public DbSet<CartProduct> CartProducts { get; set; }
         public DbSet<OrderProduct> OrderProducts { get; set; }
-        public DbSet<Comment> comments { get; set; }
-
-
+        public DbSet<Comment> Comments { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<DigitalProduct> DigitalProducts { get; set; }
         public DbSet<PhysicalProduct> PhysicalProducts { get; set; }
@@ -26,18 +28,22 @@ namespace Shopping_WebApi.Infrastructure.Data.DbContext
         {
             base.OnModelCreating(modelBuilder);
 
+            
             modelBuilder.Entity<Customer>()
                 .HasOne(c => c.Cart)
-                .WithOne(c => c.Customer)
-                .HasForeignKey<Cart>(c => c.CustomerId);
+                .WithOne(cart => cart.Customer)
+                .HasForeignKey<Cart>(cart => cart.CustomerId);
 
-
+            
             modelBuilder.Entity<Comment>()
-                .HasQueryFilter(c => !c.IsDeleted);
+                .HasQueryFilter(comment => !comment.IsDeleted);
 
+            
+            modelBuilder.Entity<User>()
+                .Property(u => u.FirstName)
+                .IsRequired(); 
 
-
-
+          
         }
     }
 }
