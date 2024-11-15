36d1959e8095f9511a1b722f6ac578187ef9dea6
YunusAlamm <alamdari.yoonos@gmail.com>
2024-11-15 21:32:11 +0330

    Update LoginUserCommandHandler.cs

--- Shopping_WebApi/Shopping_WebApi/Features/RegisterAndLogin/CommandHandlers/LoginUserCommandHandler.cs
+++ Shopping_WebApi/Shopping_WebApi/Features/RegisterAndLogin/CommandHandlers/LoginUserCommandHandler.cs
@@ -6,29 +6,24 @@ using Shopping_WebApi.Infrastructures.JwtTokenService;
 
 namespace Shopping_WebApi.Features.RegisterAndLogin.CommandHandlers
 {
-    public class LoginUserCommandHandler : IRequestHandler<LoginUserCommand, string>
+    public class LoginUserCommandHandler(
+        UserManager<User> _userManager,
+        SignInManager<User> _signInManager,
+        ITokenService _tokenService
+        ) : IRequestHandler<LoginUserCommand, string>
     {
-        private readonly UserManager<User> _userManager;
-        private readonly SignInManager<User> _signInManager;
-        private readonly ITokenService _tokenService; // A service to generate JWT tokens
-
-        public LoginUserCommandHandler(UserManager<User> userManager, SignInManager<User> signInManager, ITokenService tokenService)
-        {
-            _userManager = userManager;
-            _signInManager = signInManager;
-            _tokenService = tokenService;
-        }
-
         public async Task<string> Handle(LoginUserCommand request, CancellationToken cancellationToken)
         {
             var user = await _userManager.FindByEmailAsync(request.LoginDto.Email);
-            if (user == null) return null;
+            if (user == null) 
+                return null;
 
             var result = await _signInManager.CheckPasswordSignInAsync(user, request.LoginDto.Password, false);
-            if (!result.Succeeded) return null;
+            if (!result.Succeeded) 
+                return null;
 
             var roles = await _userManager.GetRolesAsync(user);
-            return _tokenService.GenerateToken(user, roles); // Generates a JWT token
+            return _tokenService.GenerateToken(user, roles); 
         }
     }
 }
