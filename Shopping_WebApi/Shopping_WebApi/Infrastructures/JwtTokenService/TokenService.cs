3acd5bd042e2b03da5a59e56fc90f760ce036245
YunusAlamm <alamdari.yoonos@gmail.com>
2024-11-15 21:36:42 +0330

    Create TokenService.cs

---
+++ Shopping_WebApi/Shopping_WebApi/Infrastructures/JwtTokenService/TokenService.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using Shopping_WebApi.Core.Entities;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Shopping_WebApi.Infrastructures.JwtTokenService
+{
+    public class TokenService(
+        IConfiguration config
+        ) : ITokenService
+    {
+        public string GenerateToken(User user, IList<string> roles)
+        {
+            var claims = new List<Claim>
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, user.Id),
+            new Claim(JwtRegisteredClaimNames.Email, user.Email),
+            new Claim("FirstName", user.FirstName),
+            new Claim("LastName", user.LastName)
+        };
+
+            claims.AddRange(roles.Select(role => new Claim(ClaimTypes.Role, role)));
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["Jwt:Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var token = new JwtSecurityToken(
+                config["Jwt:Issuer"],
+                config["Jwt:Issuer"],
+                claims,
+                expires: DateTime.Now.AddMinutes(15),
+                signingCredentials: creds);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
