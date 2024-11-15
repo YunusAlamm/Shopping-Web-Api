d2798c85d95a87793ba71d0f4dd000917abb3ac6
YunusAlamm <alamdari.yoonos@gmail.com>
2024-11-15 21:36:39 +0330

    Update ITokenService.cs

--- Shopping_WebApi/Shopping_WebApi/Infrastructures/JwtTokenService/ITokenService.cs
+++ Shopping_WebApi/Shopping_WebApi/Infrastructures/JwtTokenService/ITokenService.cs
@@ -1,47 +1,9 @@
-﻿using Microsoft.IdentityModel.Tokens;
-using Shopping_WebApi.Core.Entities;
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
-using System.Text;
+﻿using Shopping_WebApi.Core.Entities;
 
 namespace Shopping_WebApi.Infrastructures.JwtTokenService
 {
     public interface ITokenService
     {
         string GenerateToken(User user, IList<string> roles);
-    }
-
-    public class TokenService : ITokenService
-    {
-        private readonly IConfiguration _config;
-
-        public TokenService(IConfiguration config)
-        {
-            _config = config;
-        }
-
-        public string GenerateToken(User user, IList<string> roles)
-        {
-            var claims = new List<Claim>
-        {
-            new Claim(JwtRegisteredClaimNames.Sub, user.Id),
-            new Claim(JwtRegisteredClaimNames.Email, user.Email),
-            new Claim("FirstName", user.FirstName),
-            new Claim("LastName", user.LastName)
-        };
-
-            claims.AddRange(roles.Select(role => new Claim(ClaimTypes.Role, role)));
-
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
-            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
-            var token = new JwtSecurityToken(
-                _config["Jwt:Issuer"],
-                _config["Jwt:Issuer"],
-                claims,
-                expires: DateTime.Now.AddMinutes(15),
-                signingCredentials: creds);
-
-            return new JwtSecurityTokenHandler().WriteToken(token);
-        }
     }
 }
