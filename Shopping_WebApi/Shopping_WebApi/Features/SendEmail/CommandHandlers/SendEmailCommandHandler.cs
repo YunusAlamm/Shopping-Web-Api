459d290dfa09aa3840fb214e7a5eba78a03a2686
YunusAlamm <alamdari.yoonos@gmail.com>
2024-11-15 21:35:37 +0330

    Update SendEmailCommandHandler.cs

--- Shopping_WebApi/Shopping_WebApi/Features/SendEmail/CommandHandlers/SendEmailCommandHandler.cs
+++ Shopping_WebApi/Shopping_WebApi/Features/SendEmail/CommandHandlers/SendEmailCommandHandler.cs
@@ -1,17 +1,17 @@
-﻿//using MediatR;
-//using Microsoft.AspNetCore.Identity.UI.Services;
-//using Shopping_WebApi.Features.SendEmail.Commands;
+﻿using MediatR;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Shopping_WebApi.Features.SendEmail.Commands;
 
-//namespace Shopping_WebApi.Features.SendEmail.CommandHandlers
-//{
-//    public class SendEmailCommandHandler(
-//        IEmailSender _emailSender
-//        ) : IRequestHandler<SendEmailCommand, bool>
-//    {
-//        public async Task<bool> Handle(SendEmailCommand request, CancellationToken cancellationToken)
-//        {
-//            await _emailSender.SendEmailAsync(request.ToEmail, request.Subject, request.Body);
-//            return true;
-//        }
-//    }
-//}
+namespace Shopping_WebApi.Features.SendEmail.CommandHandlers
+{
+    public class SendEmailCommandHandler(
+        IEmailSender _emailSender
+        ) : IRequestHandler<SendEmailCommand, bool>
+    {
+        public async Task<bool> Handle(SendEmailCommand request, CancellationToken cancellationToken)
+        {
+            await _emailSender.SendEmailAsync(request.ToEmail, request.Subject, request.Body);
+            return true;
+        }
+    }
+}
