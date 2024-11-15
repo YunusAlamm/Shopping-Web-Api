35c60cb9b0d28f63e2528f1b42de39a13194da6e
YunusAlamm <alamdari.yoonos@gmail.com>
2024-11-15 21:32:25 +0330

    Update RegisterUserCommand.cs

--- Shopping_WebApi/Shopping_WebApi/Features/RegisterAndLogin/Commands/RegisterUserCommand.cs
+++ Shopping_WebApi/Shopping_WebApi/Features/RegisterAndLogin/Commands/RegisterUserCommand.cs
@@ -4,12 +4,10 @@ using Shopping_WebApi.Features.RegisterAndLogin.Dto;
 
 namespace Shopping_WebApi.Features.RegisterAndLogin.Commands
 {
-    public class RegisterUserCommand : IRequest<IdentityResult>
+    public class RegisterUserCommand(
+        RegisterUserDto userDto
+        ) : IRequest<IdentityResult>
     {
-        public RegisterUserDto UserDto { get; set; }
-        public RegisterUserCommand(RegisterUserDto userDto)
-        {
-            UserDto = userDto;
-        }
+        public RegisterUserDto UserDto { get; set; } = userDto;
     }
 }
