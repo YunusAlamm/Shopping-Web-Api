fc92705de270909105d1f2d110de4f89ff29f225
Yunus Alamdari <alamdari.yoonos@gmail.com>
2024-11-29 00:34:12 +0330

    Update DigitalProductMapping.cs
    config the mapper to use resolver for recieving names of categories

--- Shopping_WebApi/Shopping_WebApi/Features/DigitalProducts/Mapping/DigitalProductMapping.cs
+++ Shopping_WebApi/Shopping_WebApi/Features/DigitalProducts/Mapping/DigitalProductMapping.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using Shopping_WebApi.Core.Entities;
 using Shopping_WebApi.Features.DigitalProducts.Dto;
+using Shopping_WebApi.Features.DigitalProducts.Mapping.Resolvers;
 
 namespace Shopping_WebApi.Features.DigitalProducts.Mapping
 {
@@ -10,8 +11,12 @@ namespace Shopping_WebApi.Features.DigitalProducts.Mapping
     {
         public DigitalProductMapping()
         {
+            CreateMap<DigitalProduct, DigitalProductDto>()
+                .ForMember(dest => dest.CategoryNames,
+                           opt => opt.MapFrom<CategoryNamesResolver>());
+
             CreateMap<DigitalProductDto, DigitalProduct>();
-            CreateMap<DigitalProduct, DigitalProductDto>();
+            
 
 
 
