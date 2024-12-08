cade91ad5e1ca653efcf96c14ae817ae4dfde991
YunusAlamm <alamdari.yoonos@gmail.com>
2024-12-08 10:01:04 +0330

    Update Category.cs

--- Shopping_WebApi/Shopping_WebApi/Core/Entities/Category.cs
+++ Shopping_WebApi/Shopping_WebApi/Core/Entities/Category.cs
@@ -1,8 +1,11 @@
-﻿namespace Shopping_WebApi.Core.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shopping_WebApi.Core.Entities
 {
     public class Category
     {
         public Guid Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public List<Product>? Products { get; set; }
     }
